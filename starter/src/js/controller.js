f6a5b0f0637fc78866999300f845c0bba51683d5
selajdin89 <selajdinmemeti89@gmail.com>
2022-03-17 16:18:41 +0100

    Added update

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -119,7 +119,6 @@ const init = function () {
   searchViews.adHandlerSearch(controlSearchResults);
   paginationView.adHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('It works fine');
 };
 init();
 
