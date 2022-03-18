713598bca597cb394d229231b7c21c05ce17f194
selajdin89 <selajdinmemeti89@gmail.com>
2022-03-18 09:20:17 +0100

    message

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -119,6 +119,7 @@ const init = function () {
   searchViews.adHandlerSearch(controlSearchResults);
   paginationView.adHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  console.log('Yes it works');
 };
 init();
 
